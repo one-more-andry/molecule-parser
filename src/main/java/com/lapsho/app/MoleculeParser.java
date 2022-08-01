db1aef6d1cc36c12599f3229e100e23a3d0d9a22
Andrew Pavlovsky <pavlowskiandrew@gmail.com>
2022-08-01 08:28:51 +0300

    feat: initiated project

---
+++ src/main/java/com/lapsho/app/MoleculeParser.java
@@ -0,0 +1,13 @@
+package com.lapsho.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class MoleculeParser
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/lapsho/app/MoleculeParserTest.java
@@ -0,0 +1,20 @@
+package com.lapsho.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MoleculeParserTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
